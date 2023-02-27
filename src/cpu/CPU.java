131304dea691920cb9b8de7ab1c0938a2247d7c4
Timofte Maxim <timoftemaximlink@gmail.com>
2023-02-27 12:50:39 +0200

    Initial commit

---
+++ src/cpu/CPU.java
@@ -0,0 +1,13 @@
+package cpu;
+
+public class CPU {
+    class Processor {
+
+    }
+    class RAM {
+
+    }
+    Processor processor = new Processor();
+    CPU cpu = new CPU();
+    RAM ram = new RAM();
+}
---
+++ src/cpu/Motherboard.java
@@ -0,0 +1,12 @@
+package cpu;
+
+public class Motherboard {
+    static class USB {
+        static void USBinsert() {
+            USB usb = new USB();
+            usb.USBinsert();
+            Motherboard motherboard = new Motherboard();
+
+        }
+    }
+}
